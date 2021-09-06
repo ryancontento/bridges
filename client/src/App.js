fd29cffee24845719b5f063f861c18a5891602da
Ryan Contento <contento.rk@gmail.com>
2021-09-06 14:38:30 -0400

    basic static page

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>Are they open?</h1>
+      <h2>Cape Fear Memorial Bridge</h2>
+        <p>Status:</p>
+    <h2>Isabel Holmes Bridge</h2>
+      <p>Status:</p>
+    
+    </div>
+  );
+}
+
+export default App;
