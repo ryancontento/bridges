5d1ce5279022108e29ac3ea3b995863b78b8dc24
zacharytyhacz <zactyh@gmail.com>
2021-09-06 16:38:58 -0400

    Added checking if a tweet is 'old'

--- server/utils/tweetChecker.js
+++ server/utils/tweetChecker.js
@@ -2,15 +2,28 @@
 //
 // @returns 
 const parseTweet = (inTweet) => {
-  console.log('in tweet:', inTweet)
   const words = inTweet.text.split(' ').map((s) => s.replace('.',''))
+
+  // both of these times are numbers in seconds
+  const tweetCreatedAt = new Date(inTweet.created_at).getTime()
+  const now = Date.now()
+
+  const tweetCreatedHoursAgo = Math.abs(now - tweetCreatedAt) / 36e5;
+  const isTweetOld = tweetCreatedHoursAgo > 0.5
 
   for(let i = 0; i < words.length; i++) {
     if (words[i].trim().toLowerCase() === 'now') {
       const nextWord = words[i+1].trim().toLowerCase()
+      const isOpen = nextWord === 'opening'
+
+      if (isOpen && isTweetOld) {
+        return {
+          open: false
+        }
+      }
       
       return {
-        open: nextWord === 'opening'
+        open: isOpen
       }
     }
   }
