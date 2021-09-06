0e7c11107a6e55eb4208cc7c81d631747a9e52e3
Ryan Contento <contento.rk@gmail.com>
2021-09-06 16:10:58 -0400

    basic interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+    
+    function App() {
   return (
     <div>
-    <h1>Are they open?</h1>
-      <h2>Cape Fear Memorial Bridge</h2>
-        <p>Status:</p>
-    <h2>Isabel Holmes Bridge</h2>
-      <p>Status:</p>
-    
+      <h1>Are They open?</h1>
+        <h2>Cape Fear Memorial</h2>
+          <p>Status:</p>
+        <h2>Isabel Holmes Bridge</h2>
+          <p>Status:</p>
     </div>
   );
 }
 
 export default App;
+
+
