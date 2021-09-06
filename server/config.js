0457222a807eda06b390341725aebfcd651e1a0a
zacharytyhacz <zactyh@gmail.com>
2021-09-06 14:43:43 -0400

    Added config parsing of twitter envs

--- server/config.js
+++ server/config.js
@@ -10,6 +10,10 @@ const config = () => {
   })
 
   return {
+    TWITTER_API_KEY: process.env.TWITTER_API_KEY,
+    TWITTER_API_KEY_SECRET: process.env.TWITTER_API_KEY_SECRET,
+    TWITTER_ACCESS_TOKEN: process.env.TWITTER_ACCESS_TOKEN,
+    TWITTER_ACCESS_TOKEN_SECRET: process.env.TWITTER_ACCESS_TOKEN_SECRET,
     TWITTER_BEARER_TOKEN: process.env.TWITTER_BEARER_TOKEN
   }
 }
